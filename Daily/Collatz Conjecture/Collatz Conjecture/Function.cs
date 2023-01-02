cd211f025441f2bef4897eabc299de3dc659bdd4
MagmaArcade <102445046+MagmaArcade@users.noreply.github.com>
2023-01-02 22:26:33 +1100

    day 2

---
+++ Daily/Collatz Conjecture/Collatz Conjecture/Function.cs
@@ -0,0 +1,50 @@
+﻿// Created By MagmaAracde
+// Date: 2/1/23
+
+namespace Collatz_Conjecture
+{
+    public class Func
+    {
+        List<int> History = new List<int>();
+        int Target;
+
+        public Func()
+        {
+            Console.WriteLine("This program explores the Collatz Conjecture. The conjecture asks whether repeating two \nsimple arithmetic operations will eventually transform every positive integer into 1");
+            Console.WriteLine();
+            Console.WriteLine("What is your target number?");
+            Target = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+            // calculation: check whether the current number is even or odd
+            // this will affect what calculation is applied to the number
+            int currentNum = Target;
+            while (currentNum != 1)
+            {
+                if (currentNum % 2 == 0)
+                {
+                    //number is even
+                    currentNum = currentNum / 2;
+                    History.Add(currentNum);
+                }
+                else
+                {
+                    // number is odd
+                    currentNum = (3 * currentNum) + 1;
+                    History.Add(currentNum);
+                }
+            }
+        }
+
+
+        // print: the target number and all iterations made to reach the final number of 1
+        public void Print()
+        {
+           Console.WriteLine("input: " + Target);
+           Console.WriteLine("output: [" + string.Join(", ", History) + "]");
+        }
+    }
+}
+    
+
---
+++ Daily/Collatz Conjecture/Collatz Conjecture/Program.cs
@@ -0,0 +1,18 @@
+﻿// Created By MagmaAracde
+// Date: 2/1/23
+
+// This program explores the Collatz Conjecture. The conjecture asks whether repeating two \nsimple arithmetic operations will eventually
+// transform every positive integer into 1
+namespace Collatz_Conjecture
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Func myFunc;
+            myFunc = new Func();
+
+            myFunc.Print();
+        }
+    }
+}
