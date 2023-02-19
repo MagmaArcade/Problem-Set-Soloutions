3fbdf5ee79bcb886dfdb26eb9ab3ceb7a355c5a5
MagmaArcade <102445046+MagmaArcade@users.noreply.github.com>
2023-02-19 16:33:47 +1100

    challanges

---
+++ Tester/Tester/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Identify_Longest_Palindromic_Substring
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Func myFunc;
+            myFunc = new Func();
+        }
+    }
+}
